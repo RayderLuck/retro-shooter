58dfb4e78149b7311a1c845595a2b32a6010e689
Rayder <lucastrindaderayder@hotmail.com>
2026-08-04 16:51:18 -0300

    Refactor PowerUp class properties and drawing logic

--- powerup.js
+++ powerup.js
@@ -2,36 +2,37 @@ export class PowerUp {
     constructor(x, y, type) {
         this.x = x;
         this.y = y;
-        this.width = 24;
-        this.height = 24;
-        this.type = type; // 'moeda', 'explosivo', 'perfurante', 'ricochete'
-        this.speed = 3;
+        this.width = 20;
+        this.height = 20;
+        this.type = type; // 'duplo', 'triplo', 'velocidade' ou 'moeda'
+        this.speed = 2;
     }
 
-    // Faz o item descer pela tela
     update() {
-        this.y += this.speed;
+        // Move o power-up da direita para a esquerda junto com os itens
+        this.x -= this.speed;
     }
 
-    // Desenha o item (você pode customizar cores ou usar sprites depois)
     draw(ctx) {
         ctx.save();
         if (this.type === 'moeda') {
-            ctx.fillStyle = '#ffd700'; // Dourado para moedas
+            ctx.fillStyle = '#ffff00'; // Amarelo para moeda
             ctx.beginPath();
-            ctx.arc(this.x + this.width / 2, this.y + this.height / 2, this.width / 2, 0, Math.PI * 2);
+            ctx.arc(this.x + 10, this.y + 10, 10, 0, Math.PI * 2);
             ctx.fill();
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = '#ffaa00';
-            ctx.stroke();
-        } else {
-            ctx.fillStyle = '#ff00ff'; // Magenta para power-ups de tiro
+        } else if (this.type === 'duplo') {
+            ctx.fillStyle = '#00ff00'; // Verde para Tiro Duplo
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        } else if (this.type === 'triplo') {
+            ctx.fillStyle = '#ff00ff'; // Roxo/Magenta para Tiro Triplo
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        } else if (this.type === 'velocidade') {
+            ctx.fillStyle = '#00ffff'; // Ciano para Velocidade
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
         ctx.restore();
     }
 
-    // Verifica colisão com a nave do jogador
     checkCollision(player) {
         return (
             player.x < this.x + this.width &&
